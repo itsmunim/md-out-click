4c12550f7ed892f1317e7ad43bc9866748d6a043
dibosh <dibosh@newscred.com>
2018-02-08 00:56:28 +0600

    Add tests for the directive

---
+++ test/out.click.directive.spec.js
@@ -0,0 +1,59 @@
+describe('mdOutClick', function () {
+  'use strict';
+
+  var $scope, element, $body, $document, $compile;
+  var NO_CLICK = 0;
+  var ELEMENT_CLICK = 1;
+  var OUTSIDE_CLICK = 2;
+
+  function _compileDirective() {
+    var template = '<button ng-click="clickState = 1" out-click="clickState = 2"></button>';
+    $scope.clickState = NO_CLICK;
+    element = $compile(template)($scope);
+    $scope.$digest();
+  }
+
+  beforeEach(module('mdOutClick'));
+
+  beforeEach(inject(function ($rootScope, _$document_, _$compile_) {
+    $compile = _$compile_;
+    $document = _$document_;
+    $scope = $rootScope.$new();
+
+    $body = {
+      eventHandlers: {},
+      click: function () {
+        var self = this;
+        self.eventHandlers.click();
+      },
+      on: function (eventName, handler) {
+        var self = this;
+        self.eventHandlers[eventName] = handler;
+      }
+    };
+
+    spyOn($document, 'find').and.callFake(function () {
+      return $body;
+    });
+  }));
+
+  describe('before clicking, click on element and outside the element', function () {
+    beforeEach(function () {
+      _compileDirective();
+    });
+
+    it('should keep the click state defined in scope as it is', function () {
+      expect($scope.clickState).toBe(NO_CLICK);
+    });
+
+    it('should change the click state so that it reflects the click was on element', function () {
+      element.triggerHandler('click');
+      expect($scope.clickState).toBe(ELEMENT_CLICK);
+    });
+
+    it('should change the click state so that it reflects the click was outside the element', function () {
+      $body.click();
+      expect($scope.clickState).toBe(OUTSIDE_CLICK);
+    });
+  });
+});
