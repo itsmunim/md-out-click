c0950e7342987f61e8992a81726f33d55a1f408e
dibosh <dibosh@newscred.com>
2018-02-08 23:38:43 +0600

    Export the module

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import outClick from './out.click.directive';
 angular.module('mdOutClick', []);
 angular.module('mdOutClick')
   .directive('onOutClick', outClick);
+
+export default angular.module('mdOutClick');
