0e17948d523d5177993e7c886bd51e6b77c2a42e
dibosh <dibosh@newscred.com>
2018-02-11 00:56:38 +0600

    Rename examples -> www and define github page

---
+++ www/index.js
@@ -0,0 +1,60 @@
+angular.module('dropdownDemo', [
+  'mdOutClick',
+  'hljs'
+]);
+
+angular.module('dropdownDemo')
+  .controller('HomeController', HomeController);
+
+angular.module('dropdownDemo')
+  .controller('ExampleController', ExampleController);
+
+
+HomeController.$inject = ['$scope'];
+
+function HomeController($scope) {
+  $scope.routing = {
+    current: '/',
+    routes: {
+      '/': 'www/home.html',
+      '/examples': 'www/examples.html'
+    },
+    navigateTo: function (route) {
+      this.current = route;
+    }
+  };
+}
+
+
+ExampleController.$inject = ['$scope'];
+
+function ExampleController($scope) {
+  $scope.examples = [
+    {
+      name: 'Dropdowns',
+      templateUrl: 'www/dropdown.demo.html'
+    },
+    {
+      name: 'Side Navigation',
+      templateUrl: 'www/side.nav.demo.html'
+    }
+  ];
+
+  $scope.selectedExampleIndex = 0;
+
+  $scope.selectExample = function (index) {
+    $scope.selectedExampleIndex = index;
+    $scope.hideDropdown();
+  };
+
+  // dropdown specific
+  $scope.isDropdownVisible = false;
+
+  $scope.hideDropdown = function () {
+    $scope.isDropdownVisible = false;
+  };
+
+  $scope.showDropdown = function () {
+    $scope.isDropdownVisible = true;
+  };
+}
